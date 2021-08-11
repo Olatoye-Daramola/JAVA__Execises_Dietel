aff6a03ff626d945c998f6c409355cb10aaac777
Olatoye Daramola <daramolatoye@gmail.com>
2021-08-11 09:17:27 +0100

    Add files via upload

--- JAVA__loops/FourPointThreeEight.java
+++ JAVA__loops/FourPointThreeEight.java
@@ -1,41 +1,23 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class FourPointThreeEight{
+	public static void main(String[] args) {
+		
+	Encrypt driver = new Encrypt();
+	Decrypt driver2 = new Decrypt();
 
-	int firstNumber, secondNumber, thirdNumber, fourthNumber, temp;
-	
-//	Scanner scan = new Scanner(System.in);
-	
-//	System.out.println("Enter your message");
-//	int userMessage = scan.nextInt();
-		
-	public void encryptMessage(int userMessage){
-	
-//		userMessage = message;
-		temp = userMessage;
+	Scanner scan = new Scanner(System.in);
 
-//		temp = message;
+	System.out.println("Enter your message");
+	String userMessage = scan.next();
+	
+	while(userMessage.length() != 4){
+		System.out.println("Enter your fucking message");
+		userMessage = scan.next();
+	}
+	driver.encryptMessage(userMessage);
+//	driver2.decryptMessage(userMessage);
+	
 		
-		fourthNumber = temp % 10;
-		fourthNumber = fourthNumber + 7;
-		fourthNumber = fourthNumber % 10;
-		temp /= 10;
-		
-		thirdNumber = temp % 10;
-		thirdNumber = thirdNumber + 7;
-		thirdNumber = thirdNumber % 10;
-		temp /= 10;
-		
-		secondNumber = temp % 10;
-		secondNumber = secondNumber + 7;
-		secondNumber = secondNumber % 10;
-		temp /= 10;
-		
-		firstNumber = temp;
-		firstNumber = firstNumber + 7;
-		firstNumber = firstNumber % 10;
-		
-		String concatMessage = (String) "" + thirdNumber + fourthNumber + firstNumber + secondNumber;
-		System.out.println(concatMessage);
 	}
 }
