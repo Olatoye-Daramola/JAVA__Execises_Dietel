85ed3eeaa86a206b932175ead2fe4a6a11eefbeb
Olatoye Daramola <daramolatoye@gmail.com>
2021-08-29 22:32:21 +0100

    Add files via upload

---
+++ ChapNine/eStore/BillingInformation.java
@@ -0,0 +1,7 @@
+package eStore;
+
+public class BillingInformation extends Customers{
+    private String phoneNumber;
+    private String receiverName;
+    private Address deliveryAddress;
+}
---
+++ ChapNine/eStore/CreditCardInformation.java
@@ -0,0 +1,9 @@
+package eStore;
+
+public class CreditCardInformation extends BillingInformation{
+    private int cvv;
+    private int expirationYear;
+    private int expirationMonth;
+    private int creditCardNumber;
+    private String nameOnCard;
+}
