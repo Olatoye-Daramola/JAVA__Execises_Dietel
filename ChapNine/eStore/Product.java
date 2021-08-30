6badf860f53182e42bfbb1f3f1dc32e69a475677
Olatoye Daramola <daramolatoye@gmail.com>
2021-08-30 13:21:15 +0100

    Add files via upload

---
+++ ChapNine/eStore/Product.java
@@ -0,0 +1,11 @@
+package eStore;
+
+import java.math.BigDecimal;
+
+public class Product{
+    private int productId;
+    private String productName;
+    private BigDecimal price;
+    private String productDescription;
+    private ProductCategory productCategory;
+}
