1390f91d27aaddf31d8b3c4beb59fe0e2208e7bc
Olatoye Daramola <daramolatoye@gmail.com>
2021-08-30 13:21:59 +0100

    Add files via upload

---
+++ ChapNine/eStore/User.java
@@ -0,0 +1,10 @@
+package eStore;
+
+public class User{
+    private int age;
+    private String name;
+    private String emailAddress;
+    private String password;
+    private String phoneNumber;
+    private Address homeAddress;
+}
---
+++ ChapNine/eStore/taskList.java
@@ -0,0 +1,36 @@
+/*
+**Users
+    *Customers, Sellers, Admin
+    -age
+    -name
+    -email address
+    -home address
+    -password
+    -phone
+
+    *Customers
+        --billing information
+            ---phone number
+            ---receiver name
+            ---delivery address
+                ----city name
+                ----country name
+                ----house number
+                ----street
+                ----state
+            ---credit card information
+                ----card cvv
+                ----card expiration year, month,
+                ----credit card number
+                ----name on card
+                ----card type
+                    ----MASTER, VISA, VERVE, AMERICAN EXPRESS
+        --shopping cart
+            ---items
+                ----quantity of product selected
+                ----product
+                    -----product id
+                    -----product description
+                    -----product category
+                        ------ELECTRONICS, GROCERIES, UTENSILS, CLOTHING
+*/
