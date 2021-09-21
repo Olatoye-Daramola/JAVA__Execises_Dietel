19f992eeed18035ab64c63d67e73d8969fab0905
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-21 20:39:31 +0100

    6.14

--- ChapSix/Exponentiation.java
+++ ChapSix/Exponentiation.java
@@ -1,2 +1,15 @@
 public class Exponentiation {
+    public static int integerPower(int base, int exponent) {
+        int init = 1;
+        int power = base;
+        while (init < exponent) {
+            power *= base;
+            init++;
+        }
+        return power;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(integerPower(3, 4));
+    }
 }
