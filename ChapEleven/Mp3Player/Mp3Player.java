5513d596a9135bac09742214676ee2e6bf0d25fe
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-26 18:08:59 +0100

    Mp3 Player

--- ChapEleven/Mp3Player/Mp3Player.java
+++ ChapEleven/Mp3Player/Mp3Player.java
@@ -1,4 +1,9 @@
 package Mp3Player;
 
 public class Mp3Player {
+    private  PowerButton powerButton = new PowerButton();
+
+    public PowerButton switchButton() {
+        return powerButton;
+    }
 }
