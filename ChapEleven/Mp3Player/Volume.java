3e5d12d4050d5533c88c1a58f779fcc65282ac60
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-26 19:55:37 +0100

    Mp3 Player

---
+++ ChapEleven/Mp3Player/Volume.java
@@ -0,0 +1,13 @@
+package Mp3Player;
+
+public class Volume {
+    private int volume = 25;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
