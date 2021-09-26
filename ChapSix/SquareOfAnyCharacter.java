148d3272891937ff56967b0305e0b4a0eed9604e
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-26 23:30:02 +0100

    6.19

--- ChapSix/SquareOfAnyCharacter.java
+++ ChapSix/SquareOfAnyCharacter.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class SquareOfAnyCharacter {
     public static void squareOfAnyCharacter(int numberOfSides, char fillCharacter) {
+
         for (int row = 0; row < numberOfSides; row++) {
             for (int column = 0; column < numberOfSides; column++) {
                 System.out.print(fillCharacter + "  ");
@@ -9,6 +12,10 @@ public class SquareOfAnyCharacter {
     }
 
     public static void main(String[] args) {
-        squareOfAnyCharacter(4, '$');
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter fill-character");
+        char fill = input.next().charAt(0);
+
+        squareOfAnyCharacter(4,fill);
     }
 }
