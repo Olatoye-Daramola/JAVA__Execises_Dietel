0d67852761f73833800b4c439e90957715236f64
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-26 19:54:29 +0100

    Mp3 Player

--- ChapEleven/Mp3Player/Mp3Player.java
+++ ChapEleven/Mp3Player/Mp3Player.java
@@ -1,9 +1,34 @@
 package Mp3Player;
 
+import static Mp3Player.MusicState.*;
+import static Mp3Player.PowerButtonPosition.*;
+
 public class Mp3Player {
-    private  PowerButton powerButton = new PowerButton();
+    private PowerButton powerButton = new PowerButton();
+    private MusicState musicState = STOPPED;
+    private Volume volume = new Volume();
 
     public PowerButton switchButton() {
         return powerButton;
+    }
+
+    public void switchMp3PlayerOn() {
+        powerButton.switchMp3PowerTo(ON);
+    }
+
+    public void switchMp3PlayerOff() {
+        powerButton.switchMp3PowerTo(OFF);
+    }
+
+    public MusicState getMusicPlayingState() {
+        return musicState;
+    }
+
+    public void playMusic() {
+        musicState = PLAYING;
+    }
+
+    public int getVolumeLevel() {
+        return volume.getVolume();
     }
 }
---
+++ ChapEleven/Mp3Player/PowerButtonPosition.java
@@ -0,0 +1,5 @@
+package Mp3Player;
+
+public enum PowerButtonPosition {
+    OFF, ON;
+}
