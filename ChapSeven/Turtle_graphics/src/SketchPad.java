965c6e4f81f7987b44a01bac6d877903e9423d27
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-27 16:42:52 +0100

    ...

---
+++ ChapSeven/Turtle_graphics/src/SketchPad.java
@@ -0,0 +1,24 @@
+package Turtle_graphics.src;
+
+public class SketchPad {
+    int[][] floor;
+    public SketchPad(int floorRow, int floorColumn) {
+        floor = new int[floorRow][floorColumn];
+    }
+
+    public int[][] getFloor() {
+        return floor;
+    }
+
+    public void displayFloor() {
+        for (int[] row : floor) {
+            for (int column : row) {
+                if (column == 1) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
+    }
+}
