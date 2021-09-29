40aed0a0b4e4e340722a930e38d970f45c54a2b8
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-29 04:57:33 +0100

    ...

---
+++ ChapSix/SeparatingDigits.java
@@ -0,0 +1,23 @@
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//
+//public class SeparatingDigits {
+//
+//    public static int[] getNumber() {
+//        String userNumber = "01101111";
+//
+//        final int NUMBER_OF_DIGITS_IN_USER_INPUT = 8;
+//        int index = 0;
+//        int[] userNumberToArray = new int[NUMBER_OF_DIGITS_IN_USER_INPUT];
+//
+//        while (index < NUMBER_OF_DIGITS_IN_USER_INPUT) {
+//            userNumberToArray[index] = Integer.parseInt(String.valueOf(userNumber.charAt(index)));
+//            index++;
+//        }
+//        return userNumberToArray;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(getNumber()));
+//    }
+//}
