51a9777131d253ed0946743108b3643e5dd0b4a5
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-29 05:45:54 +0100

    Refactored

--- Other_others/SevenSegmentDisplay/SevenSegmentDisplayDriver.java
+++ Other_others/SevenSegmentDisplay/SevenSegmentDisplayDriver.java
@@ -1,4 +1,16 @@
 package SevenSegmentDisplay;
 
+import java.util.Scanner;
+
 public class SevenSegmentDisplayDriver {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String userInput = scanner.next();
+        String userInput = "01101111";
+
+        SevenSegmentDisplay sevenSegmentDisplay = new SevenSegmentDisplay();
+
+        SevenSegmentDisplay.setScreen(userInput);
+        SevenSegmentDisplay.display();
+    }
 }
