e0c9da448a70e618f8d3c1c9c2e36cf8a080458a
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-01 15:06:59 +0100

    ...destroyed, to be rewritten...

--- Other_others/SevenSegmentDisplay/SevenSegmentDisplay.java
+++ Other_others/SevenSegmentDisplay/SevenSegmentDisplay.java
@@ -8,57 +8,54 @@ import java.util.InputMismatchException;
 public class SevenSegmentDisplay {
 
     public static final int[][] SCREEN = new int[5][4];
-    private static final int NUMBER_OF_DIGITS_IN_USER_INPUT = 8;
-    private static final int[] ARRAY_FROM_USER_INPUT = new int[NUMBER_OF_DIGITS_IN_USER_INPUT];
 
     public static void getUserInput(String userInput) {
-        if (userInput.length() <= 8) {
-            for (int index = 0; index < NUMBER_OF_DIGITS_IN_USER_INPUT; index++) {
-                if (Character.getNumericValue(userInput.charAt(index)) == 0 ||
-                        Character.getNumericValue(userInput.charAt(index)) == 1) {
-                    ARRAY_FROM_USER_INPUT[index] = Integer.parseInt(String.valueOf(userInput.charAt(index)));
-                } else {
-                    throw new InputMismatchException("Enter 0s and 1s only");
-                }
-            }
-        } else {
-            throw new ArrayIndexOutOfBoundsException("8-bit binary number only");
+        if (userInput.length() > 8) throw new ArrayIndexOutOfBoundsException("8-bit binary number only");
+        else {
+            userInput = addTrailingZerosTo(userInput);
         }
+        for (int index = 0; index < userInput.length(); index++) {
+            char character = userInput.charAt(index);
+            if (character == 0 || character == 1) throw new InputMismatchException("Enter 0s and 1s only");
+        }
+    }
+
+    private static String addTrailingZerosTo(String userInput) {
+        int input = Integer.parseInt(userInput);
+        userInput = String.format("%08d", input);
+        return userInput;
     }
 
     public static void setScreen(String userInput) {
         getUserInput(userInput);
         for (int[] row : SCREEN) {
             for (int column = 0; column < row.length; column++) {
-                if (ARRAY_FROM_USER_INPUT[7] == 1) {
-                    fillSegments();
-                } else {
-                    clearScreen();
-                }
+                if (userInput.charAt(7) == 1) fillSegments(userInput);
+                else clearScreen();
             }
         }
     }
 
-    private static void fillSegments() {
-        if (ARRAY_FROM_USER_INPUT[0] == 1) {
+    private static void fillSegments(String userInput) {
+        if (userInput.charAt(0) == 1) {
             firstSegment();
         }
-        if (ARRAY_FROM_USER_INPUT[1] == 1) {
+        if (userInput.charAt(1) == 1) {
             secondSegment();
         }
-        if (ARRAY_FROM_USER_INPUT[2] == 1) {
+        if (userInput.charAt(2) == 1) {
             thirdSegment();
         }
-        if (ARRAY_FROM_USER_INPUT[3] == 1) {
+        if (userInput.charAt(3) == 1) {
             fourthSegment();
         }
-        if (ARRAY_FROM_USER_INPUT[4] == 1) {
+        if (userInput.charAt(4) == 1) {
             fifthSegment();
         }
-        if (ARRAY_FROM_USER_INPUT[5] == 1) {
+        if (userInput.charAt(5) == 1) {
             sixthSegment();
         }
-        if (ARRAY_FROM_USER_INPUT[6] == 1) {
+        if (userInput.charAt(6) == 1) {
             seventhSegment();
         }
     }
@@ -127,8 +124,3 @@ public class SevenSegmentDisplay {
         }
     }
 }
-
-//Test that each segment prints
-//Test that input cannot be more than eight
-//Test that input is truly binary
-//Test that it can add leading zeros
