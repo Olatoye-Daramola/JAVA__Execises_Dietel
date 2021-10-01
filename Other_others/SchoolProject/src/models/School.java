c4b94be614757f8e8350551a81d4b07d9f06d2bc
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-01 15:28:29 +0100

    ...

--- Other_others/SchoolProject/src/models/School.java
+++ Other_others/SchoolProject/src/models/School.java
@@ -1,4 +1,17 @@
 package SchoolProject.src.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//Creating Retrieving Updating Deleting = CRUD
+
 public class School {
+    private long id;
+    private String name;
+    private String description;
+    private SchoolType schoolType;
+    private List<Class> classes = new ArrayList<>();
+    private List<Staff> staffs = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
+    private Address address;
 }
