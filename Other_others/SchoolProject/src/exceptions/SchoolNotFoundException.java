1f9a27a8c602f947daf86773edc7e60dc4f19303
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-01 16:49:46 +0100

    Custom Exception created

---
+++ Other_others/SchoolProject/src/exceptions/SchoolNotFoundException.java
@@ -0,0 +1,19 @@
+package SchoolProject.src.exceptions;
+
+public class SchoolNotFoundException extends Throwable {
+    public SchoolNotFoundException() {
+        super();
+    }
+
+    public SchoolNotFoundException(String message) {
+        super(message);
+    }
+
+    public SchoolNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SchoolNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
