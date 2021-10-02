419a7e328f7ec3bce2c0ea7401a20b03c84a8855
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-02 21:05:43 +0100

    Credit Card Number Validation

---
+++ ChapEleven/CreditCardNumberValidation/test/CreditCardNumberValidatorTest.java
@@ -0,0 +1,31 @@
+package CreditCardNumberValidation.test;
+
+import CreditCardNumberValidation.src.CreditCardNumberValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreditCardNumberValidatorTest {
+
+    @Test
+    void testThatCreditCardNumberLengthIsNotLessThan13() {
+        assertFalse(CreditCardNumberValidator.isWithinValidRange(123456789123L));
+    }
+
+    @Test
+    void testThatCreditCardNumberLengthIsNotMoreThan16() {
+        assertFalse(CreditCardNumberValidator.isWithinValidRange(12345678912345678L));
+    }
+
+    @Test
+    void testThatCreditCardNumberLengthIsWithinTheRangeOf13And16() {
+        assertTrue(CreditCardNumberValidator.isWithinValidRange(12345678912345L));
+        assertTrue(CreditCardNumberValidator.isWithinValidRange(123456789123456L));
+    }
+
+    @Test
+    void testThatCreditCardNumberLengthOf13And16IsInValidRange() {
+        assertTrue(CreditCardNumberValidator.isWithinValidRange(1234567891234L));
+        assertTrue(CreditCardNumberValidator.isWithinValidRange(1234567891234567L));
+    }
+}
