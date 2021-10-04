59be1f163009dae646a6d399ada87de824825f75
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-04 21:23:27 +0100

    ...

--- ChapEleven/Mp3Player/Mp3PlayerTest.java
+++ ChapEleven/Mp3Player/Mp3PlayerTest.java
@@ -3,6 +3,9 @@ package Mp3Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Time;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import static Mp3Player.MusicState.*;
@@ -13,26 +16,26 @@ class Mp3PlayerTest {
 
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         sony = new Mp3Player();
     }
 
     @Test
     @DisplayName("Mp3 player is switched off by default")
-    public void testThatMp3IsOffByDefault(){
+    void testThatMp3IsOffByDefault(){
         assertFalse(sony.getOn());
     }
 
     @Test
     @DisplayName("Mp3 player has been turned on")
-    public void testThatMp3CanBeTurnedOn(){
+    void testThatMp3CanBeTurnedOn(){
         sony.isOn();
         assertTrue(sony.getOn());
     }
 
     @Test
     @DisplayName("Mp3 player has been turned off")
-    public void testThatMp3CanBeTurnedOff(){
+    void testThatMp3CanBeTurnedOff(){
         sony.isOn();
         assertTrue(sony.getOn());
         sony.isOn();
@@ -41,14 +44,14 @@ class Mp3PlayerTest {
 
     @Test
     @DisplayName("Mp3 player is not playing music by default")
-    public void testThatMusicIsStoppedByDefault(){
+    void testThatMusicIsStoppedByDefault(){
         sony.isOn();
         assertEquals(STOPPED, sony.getMusicPlayingState());
     }
 
     @Test
     @DisplayName("Mp3 player can play music")
-    public void testThatMp3CanPlayMusic(){
+    void testThatMp3CanPlayMusic(){
         sony.isOn();
         assertEquals(STOPPED, sony.getMusicPlayingState());
         sony.playMusic();
@@ -57,20 +60,20 @@ class Mp3PlayerTest {
 
     @Test
     @DisplayName("Mp3 player has default volume")
-    public void testThatMp3PlayerHasVolumeButton(){
+    void testThatMp3PlayerHasVolumeButton(){
         assertEquals(25, sony.getVolumeLevel());
     }
 
     @Test
     @DisplayName("Mp3 player can increase volume")
-    public void testThatMp3PlayerCanIncreaseVolume(){
+    void testThatMp3PlayerCanIncreaseVolume(){
         sony.increaseVolume();
         assertEquals(30, sony.getVolumeLevel());
     }
 
     @Test
     @DisplayName("Mp3 player cannot increase volume beyond 100")
-    public void testThatMp3PlayerCanIncreaseVolumeBeyond100(){
+    void testThatMp3PlayerCanIncreaseVolumeBeyond100(){
         for(int i = 0; i < 30; i++) {
             sony.increaseVolume();
         }
@@ -79,14 +82,14 @@ class Mp3PlayerTest {
 
     @Test
     @DisplayName("Mp3 player can decrease volume")
-    public void testThatMp3PlayerCanDecreaseVolume(){
+    void testThatMp3PlayerCanDecreaseVolume(){
         sony.decreaseVolume();
         assertEquals(20, sony.getVolumeLevel());
     }
 
     @Test
     @DisplayName("Mp3 player cannot decrease volume below 0")
-    public void testThatMp3PlayerCanDecreaseVolumeBelow0(){
+    void testThatMp3PlayerCanDecreaseVolumeBelow0(){
         for(int i = 0; i < 30; i++) {
             sony.decreaseVolume();
         }
@@ -94,13 +97,39 @@ class Mp3PlayerTest {
     }
 
     @Test
-    @DisplayName("Mp3 player can pause music")
-    public void testThatMp3CanPauseMusic(){
+    @DisplayName("Mp3 player can play music")
+    void testThatMp3CanPauseMusic(){
         sony.isOn();
+        assertEquals(STOPPED, sony.getMusicPlayingState());
         sony.playMusic();
         assertEquals(PLAYING, sony.getMusicPlayingState());
-
         sony.pauseMusic();
         assertEquals(PAUSED, sony.getMusicPlayingState());
     }
+
+    @Test
+    @DisplayName("Mp3 player has a default playlist")
+    void testThatMp3HasADefaultPlaylist(){
+        assertNotNull(sony.getDefaultPlaylist());
+    }
+
+    @Test
+    @DisplayName("Mp3 can add music to playlist")
+    void testThatMp3CanAddMusicToPlaylist(){
+        sony.isOn();
+        Music song1 = new Music("song1", 1, new Time(120));
+        sony.getDefaultPlaylist().addMusic(song1);
+        Music song2 = new Music("song2", 2, new Time(200));
+        sony.getDefaultPlaylist().addMusic(song2);
+        assertEquals(2, (sony.getDefaultPlaylist()).getNumberOfSongsInPlaylist());
+    }
+
+//    @Test
+//    @DisplayName("Mp3 player can play next music")
+//    void testThatMusicCanPlayNextMusic(){
+//        sony.isOn();
+//        sony.playMusic();
+//        sony.
+//        assertEquals(NEXT, sony.playNextMusic());
+//    }
 }
