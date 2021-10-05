e74caf6117629e56545b0dd7e541764795c24bed
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-05 16:06:57 +0100

    ...

---
+++ ChapEleven/TicTacToe/src/SquarePosition.java
@@ -0,0 +1,12 @@
+package TicTacToe.src;
+
+
+public class SquarePosition {
+    private int rowPosition;
+    private int columnPosition;
+
+    public SquarePosition(int rowPosition, int columnPosition) {
+        this.rowPosition = rowPosition;
+        this.columnPosition = columnPosition;
+    }
+}
