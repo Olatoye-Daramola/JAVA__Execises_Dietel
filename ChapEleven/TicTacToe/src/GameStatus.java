0d8be850d03d144baa173b803435c644b3146e00
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-06 06:27:45 +0100

    ...

--- ChapEleven/TicTacToe/src/GameStatus.java
+++ ChapEleven/TicTacToe/src/GameStatus.java
@@ -4,20 +4,51 @@ import static TicTacToe.src.Square.EMPTY;
 
 public class GameStatus {
     Board board;
-    String[][] playBoard = board.getBoard();;
-
+    String[][] playBoard = board.getBoard();
 
     public static boolean isWon(String[][] playBoard) {
-        return (playBoard[0][0] != String.valueOf(EMPTY) && playBoard[0][0] == playBoard[1][1] && playBoard[0][0] == playBoard[2][2]) ||
-                (playBoard[0][0] != String.valueOf(EMPTY) && playBoard[0][0] == playBoard[0][1] && playBoard[0][0] == playBoard[0][2]) ||
-                (playBoard[0][0] != String.valueOf(EMPTY) && playBoard[0][0] == playBoard[1][0] && playBoard[0][0] == playBoard[2][0]) ||
+        return firstRow(playBoard) ||
+               secondRow(playBoard) ||
+               thirdRow(playBoard) ||
 
-                (playBoard[1][0] != String.valueOf(EMPTY) && playBoard[1][0] == playBoard[1][1] && playBoard[1][0] == playBoard[1][2]) ||
-                (playBoard[2][0] != String.valueOf(EMPTY) && playBoard[2][0] == playBoard[2][1] && playBoard[2][0] == playBoard[2][2]) ||
+               firstColumn(playBoard) ||
+               secondColumn(playBoard) ||
+               thirdColumn(playBoard) ||
 
-                (playBoard[0][1] != String.valueOf(EMPTY) && playBoard[0][1] == playBoard[1][1] && playBoard[0][1] == playBoard[2][1]) ||
-                (playBoard[0][2] != String.valueOf(EMPTY) && playBoard[0][2] == playBoard[1][2] && playBoard[0][2] == playBoard[2][2]) ||
+               leftDiagonal(playBoard) ||
+               rightDiagonal(playBoard);
+    }
 
-                (playBoard[0][2] != String.valueOf(EMPTY) && playBoard[0][2] == playBoard[1][1] && playBoard[0][2] == playBoard[2][0]);
+
+    public static boolean firstRow(String[][] playBoard) {
+        return playBoard[0][0] != String.valueOf(EMPTY) && playBoard[0][0] == playBoard[0][1] && playBoard[0][0] == playBoard[0][2];
+    }
+
+    public static boolean secondRow(String[][] playBoard) {
+        return playBoard[1][0] != String.valueOf(EMPTY) && playBoard[1][0] == playBoard[1][1] && playBoard[1][0] == playBoard[1][2];
+    }
+
+    public static boolean thirdRow(String[][] playBoard) {
+        return playBoard[2][0] != String.valueOf(EMPTY) && playBoard[2][0] == playBoard[2][1] && playBoard[2][0] == playBoard[2][2];
+    }
+
+    public static boolean firstColumn(String[][] playBoard) {
+        return playBoard[0][0] != String.valueOf(EMPTY) && playBoard[0][0] == playBoard[1][0] && playBoard[0][0] == playBoard[2][0];
+    }
+
+    public static boolean secondColumn(String[][] playBoard) {
+        return playBoard[0][1] != String.valueOf(EMPTY) && playBoard[0][1] == playBoard[1][1] && playBoard[0][1] == playBoard[2][1];
+    }
+
+    public static boolean thirdColumn(String[][] playBoard) {
+        return playBoard[0][2] != String.valueOf(EMPTY) && playBoard[0][2] == playBoard[1][2] && playBoard[0][2] == playBoard[2][2];
+    }
+
+    public static boolean leftDiagonal(String[][] playBoard) {
+        return playBoard[0][0] != String.valueOf(EMPTY) && playBoard[0][0] == playBoard[1][1] && playBoard[0][0] == playBoard[2][2];
+    }
+
+    public static boolean rightDiagonal(String[][] playBoard) {
+        return playBoard[0][2] != String.valueOf(EMPTY) && playBoard[0][2] == playBoard[1][1] && playBoard[0][2] == playBoard[2][0];
     }
 }
