7d58eb4e107f78bce22537c5f0f326968c25487a
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-10 20:39:21 +0100

    POST-SUBMISSION PUSH

--- ChapEleven/TicTacToe/src/Board.java
+++ ChapEleven/TicTacToe/src/Board.java
@@ -2,13 +2,15 @@ package TicTacToe.src;
 
 import java.util.Arrays;
 
+import static TicTacToe.src.Square.*;
+
 
 public class Board {
     static Square[][] arrayOfPlayBoard = new Square[3][3];
 
-    public Board(Square square) {
+    public Board() {
         for (Square[] squares : arrayOfPlayBoard) {
-            Arrays.fill(squares, square);
+            Arrays.fill(squares, EMPTY);
         }
     }
 
