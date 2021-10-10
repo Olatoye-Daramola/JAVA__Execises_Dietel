3ac859116f39379e4908f3a50ef729c547f124f9
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-10 20:39:51 +0100

    POST-SUBMISSION PUSH

--- ChapEleven/TicTacToe/src/TicTacToeDriver.java
+++ ChapEleven/TicTacToe/src/TicTacToeDriver.java
@@ -3,22 +3,20 @@ package TicTacToe.src;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import static TicTacToe.src.Square.*;
 import static TicTacToe.src.Player.*;
 
 public class TicTacToeDriver {
     private static TicTacToe ticTacToe = new TicTacToe();
-    private static Board board = new Board(EMPTY);
+    private static Board board = new Board();
     private static Square[][] playBoard = board.getBoard();
     private static int movesCounter = 0;
     private static boolean stopGame = GameStatus.isWon(playBoard) || GameStatus.isDraw(playBoard,  movesCounter);
     private static String currentPlayer = "";
 
-
     public static void main(String... args) {
 
         while (!stopGame) {
-            Player player = ticTacToe.getPlayer();
+            Player player = ticTacToe.getCurrentPlayer();
             if (player == PLAYER_ONE) currentPlayer = "Player 1";
             else currentPlayer = "Player 2";
             takeMoveFrom(player);
@@ -27,7 +25,7 @@ public class TicTacToeDriver {
             System.out.println(ticTacToe.displayBoard());
             ticTacToe.changeCurrentPlayer();
         }
-        GameStatus.getMessage(playBoard, currentPlayer, movesCounter);
+        announceGameOutcome();
     }
 
     private static void takeMoveFrom(Player player) {
@@ -38,6 +36,11 @@ public class TicTacToeDriver {
             System.err.println(es.getMessage());
             takeMoveFrom(player);
         }
+    }
+
+    private static void announceGameOutcome() {
+        GameStatus.setMessage(playBoard, currentPlayer, movesCounter);
+        System.out.println(GameStatus.getMessage());
     }
 
     private static int input(String prompt) {
