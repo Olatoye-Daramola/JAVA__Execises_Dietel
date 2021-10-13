e52ad1770e3193557aab58ad69f2acba625e4549
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-13 17:10:22 +0100

    ...

--- Maven/BankingApplication/src/main/java/africa/semicolon/BankingApplication/services/CustomerService.java
+++ Maven/BankingApplication/src/main/java/africa/semicolon/BankingApplication/services/CustomerService.java
@@ -1,11 +1,14 @@
 package africa.semicolon.BankingApplication.services;
 
-import africa.semicolon.BankingApplication.data.models.Account;
-import africa.semicolon.BankingApplication.data.models.Customer;
+import africa.semicolon.BankingApplication.dtos.responses.CustomerResponse;
+
+import java.util.List;
 
 public interface CustomerService {
 
-    String createAccount(String firstName, String lastName);
+//    String createAccount(String firstName, String lastName);
+//    String createAccount(String firstName, String lastName, String bvn);
 
-    String createAccount(String firstName, String lastName, String bvn);
+//    List<Customer> findAll();
+    List<CustomerResponse> findAll();
 }
