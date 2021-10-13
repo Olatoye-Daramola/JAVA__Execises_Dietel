518ef3995ebcdbec3359ff6741a2b01f97ccf1b5
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-13 17:21:00 +0100

    17:10

--- Maven/BankingApplication/src/main/java/africa/semicolon/BankingApplication/dtos/responses/CustomerResponse.java
+++ Maven/BankingApplication/src/main/java/africa/semicolon/BankingApplication/dtos/responses/CustomerResponse.java
@@ -1,5 +1,6 @@
 package africa.semicolon.BankingApplication.dtos.responses;
 
+import africa.semicolon.BankingApplication.data.models.Customer;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,11 @@ public class CustomerResponse {
     private String lastName;
     private String bvn;
     private int numberOfAccounts;
+
+    public CustomerResponse(Customer customer) {
+        firstName = customer.getFirstName();
+        lastName = customer.getLastName();
+        bvn = customer.getBvn();
+        numberOfAccounts = customer.getAccounts().size();
+    }
 }
