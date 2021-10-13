9c31932f120ff3ea10093430f5ec6b62890ff994
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-13 12:08:12 +0100

    refreshed push...

--- Maven/BankingApplication/src/test/java/africa/semicolon/BankingApplication/services/BankServiceImplTest.java
+++ Maven/BankingApplication/src/test/java/africa/semicolon/BankingApplication/services/BankServiceImplTest.java
@@ -44,4 +44,37 @@ class BankServiceImplTest {
         assertEquals("02", firstBankId);
         assertEquals(2, bankService.findAllBanks().size());
     }
+
+    @Test
+    void bankCanCreateAccountForCustomers_test() {
+        String gtCoId = bankService.createBank("GTCO");
+        String accountNumber = bankService.createAccount("01", "Agba", "Mon-shure");
+
+        assertEquals("010000001", accountNumber);
+    }
+
+    @Test
+    void createTwoAccountsInBank1_accountNumberShouldBe02() {
+        String gtCoId = bankService.createBank("GTCO");
+
+        String agbaAccountNumber = bankService.createAccount("01", "Agba", "Mon-shure");
+        String ajohnneAccountNumber = bankService.createAccount("01", "Ajohnne", "LogicOverflow");
+
+        assertEquals("0100000001", agbaAccountNumber);
+        assertEquals("0100000002", ajohnneAccountNumber);
+    }
+
+    @Test
+    void createTwoAccountsInBank1_andOneAccountsInBank2_accountNumberShouldBe01() {
+        String gtCoId = bankService.createBank("GTCO");
+        String secondBankId = bankService.createBank("First Bank");
+
+        String agbaAccountNumber = bankService.createAccount(gtCoId, "Agba", "Mon-shure");
+        String ajohnneAccountNumber = bankService.createAccount(gtCoId, "Ajohnne", "LogicOverflow");
+        String jerryAccountNumber = bankService.createAccount(secondBankId, "Ajerry", "BigFowl");
+
+        assertEquals("0100000001", agbaAccountNumber);
+        assertEquals("0100000002", ajohnneAccountNumber);
+        assertEquals("0200000001", jerryAccountNumber);
+    }
 }
