7c347b9def95e51868dc6f559dcc4cae9bbc1893
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-16 11:31:10 +0100

    ...

--- Maven/BankingApplication/src/main/java/africa/semicolon/BankingApplication/services/BankServiceImpl.java
+++ Maven/BankingApplication/src/main/java/africa/semicolon/BankingApplication/services/BankServiceImpl.java
@@ -2,6 +2,7 @@ package africa.semicolon.BankingApplication.services;
 
 import africa.semicolon.BankingApplication.data.models.Account;
 import africa.semicolon.BankingApplication.data.models.Bank;
+import africa.semicolon.BankingApplication.data.models.Customer;
 import africa.semicolon.BankingApplication.data.repositories.BankRepository;
 import africa.semicolon.BankingApplication.data.repositories.BankRepositoryImpl;
 import africa.semicolon.BankingApplication.dtos.requests.CreateAccountRequest;
@@ -10,6 +11,7 @@ import java.util.List;
 
 public class BankServiceImpl implements BankService {
     private BankRepository bankRepository = new BankRepositoryImpl();
+    private CustomerService customerService = new CustomerServiceImpl();
 //    private static int lastAccountNumber = 0;
 //    private static int lastBankIdCreated = 0;
 
@@ -36,9 +38,17 @@ public class BankServiceImpl implements BankService {
     public String createAccount(CreateAccountRequest createAccountRequest) {
         String bankId = createAccountRequest.getBankId();
         String accountNumber = generateSuffixFor(bankId);
+        Customer customer = new Customer();
         Account account = new Account();
         account.setNumber(bankId + accountNumber);
         account.setType(createAccountRequest.getAccountType());
+
+        customer.setFirstName(createAccountRequest.getFirstName());
+        customer.setLastName(createAccountRequest.getLastName());
+        customer.getAccounts().add(account);
+        customer.setBvn("No bvn implemented yet");
+        customerService.addNew(customer);
+
         Bank bank = bankRepository.findBankById(bankId);
         bank.getAccounts().add(account);
         bankRepository.save(bank);
