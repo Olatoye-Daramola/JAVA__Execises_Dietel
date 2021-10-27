0c9e58dc7cf2a74043cc277309e8d07051b35790
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-27 17:47:07 +0100

    ...

--- Other_others/DataStructure/src/DataStructure/Stack.java
+++ Other_others/DataStructure/src/DataStructure/Stack.java
@@ -9,28 +9,43 @@ public class Stack {
     }
 
     public void push(int element) {
-        elements[numberOfElements] = element;
-        numberOfElements++;
+        if(isFull()) throw new OverFlowException("Full");
+//        try {
+            elements[numberOfElements++] = element;
+//        }
+//        catch (ArrayIndexOutOfBoundsException e) {
+//            throw new OverFlowException("Full");
+//        }
     }
     public int size() {
         return numberOfElements;
     }
 
-    public void pop() {
+    public int pop() {
         if(isEmpty()) throw new UnderFlowException("Getat");
-        numberOfElements--;
+        return --numberOfElements;
     }
 
     public boolean isEmpty() {
         return numberOfElements == 0;
     }
 
+    public boolean isFull() {
+        return size() == elements.length;
+    }
+
     public int peek() {
+        if (isEmpty()) throw new UnderFlowException("Empty");
         return elements[numberOfElements - 1];
     }
 
     public static class UnderFlowException extends RuntimeException {
         public UnderFlowException(String message) {
+            super(message);
+        }
+    }
+    public static class OverFlowException extends ArrayIndexOutOfBoundsException {
+        public OverFlowException(String message) {
             super(message);
         }
     }
