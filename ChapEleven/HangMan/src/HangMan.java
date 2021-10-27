c5ae23df26d941d82cf338bd284898ea881af7f5
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-27 17:15:52 +0100

    HangMan game initialized...

---
+++ ChapEleven/HangMan/src/HangMan.java
@@ -0,0 +1,15 @@
+package HangMan.src;
+
+public class HangMan {
+    private static StringBuilder platform = new StringBuilder();
+
+//    public String displayPlatform() {
+//        horizontalLineOfPlatform();
+//        secondLineOfPlatform();
+//        thirdLineOfPlatform();
+//        fourthLineOfPlatform();
+//        fifthLineOfPlatform();
+//        sixthLineOfPlatform();
+//        horizontalLineOfPlatform();
+//    }
+}
