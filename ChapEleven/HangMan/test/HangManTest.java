9726b09933343b2f1689ced31d08b836c5c1fe0b
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-28 10:26:37 +0100

    ...

--- ChapEleven/HangMan/test/HangManTest.java
+++ ChapEleven/HangMan/test/HangManTest.java
@@ -3,8 +3,6 @@ package HangMan.test;
 import HangMan.src.HangMan;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class HangManTest {
     HangMan hangMan = new HangMan();
 
