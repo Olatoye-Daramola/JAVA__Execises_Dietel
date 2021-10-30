f1a23ed6df5b8d2d3c67b2d0a797ceffe9dc12a6
Olatoye Daramola <daramolatoye@gmail.com>
2021-10-30 19:42:30 +0100

    ...

---
+++ Other_others/DataStructure/src/DataStructure/LinkedList/MySinglyLinkedList.java
@@ -0,0 +1,11 @@
+package DataStructure.src.DataStructure.LinkedList;
+
+public class MySinglyLinkedList {
+    Node<String> head;
+
+    public Node<String> getHead() {
+        return head;
+    }
+
+
+}
