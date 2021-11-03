43ef51ede233211b8894c2949a11e7f5f6e6986f
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-03 18:48:28 +0100

    ...

--- Maven/cheetah/src/main/java/africa/semicolon/cheetah/controllers/PackageController.java
+++ Maven/cheetah/src/main/java/africa/semicolon/cheetah/controllers/PackageController.java
@@ -1,15 +1,24 @@
 package africa.semicolon.cheetah.controllers;
 
+import africa.semicolon.cheetah.data.models.Package;
 import africa.semicolon.cheetah.dtos.requests.AddPackageRequest;
 import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
 import africa.semicolon.cheetah.services.PackageService;
 import africa.semicolon.cheetah.services.PackageServiceImpl;
+import org.springframework.web.bind.annotation.*;
 
+
+@RestController
 public class PackageController {
     private final PackageService packageService = new PackageServiceImpl();
 
-    public AddPackageResponse addPackage(AddPackageRequest addPackageRequest) {
+    @PostMapping("/api/addpackage")
+    public AddPackageResponse addPackage(@RequestBody AddPackageRequest addPackageRequest) {
         return packageService.addPackage(addPackageRequest);
+    }
 
+    @GetMapping("/api/package/{trackingNumber}")
+    public Package findPackageByTrackingNumber(@PathVariable("trackingNumber") Integer trackingNumber) {
+        return packageService.findMyPackageWithMy(trackingNumber);
     }
 }
