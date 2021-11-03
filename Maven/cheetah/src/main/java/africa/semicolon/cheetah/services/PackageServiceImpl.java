d7655cb21f64ff2cdba9f3d0dc01827ae9fff9ee
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-03 17:31:28 +0100

    ...

--- Maven/cheetah/src/main/java/africa/semicolon/cheetah/services/PackageServiceImpl.java
+++ Maven/cheetah/src/main/java/africa/semicolon/cheetah/services/PackageServiceImpl.java
@@ -5,6 +5,7 @@ import africa.semicolon.cheetah.data.repositories.PackageRepository;
 import africa.semicolon.cheetah.data.repositories.PackageRepositoryImpl;
 import africa.semicolon.cheetah.dtos.requests.AddPackageRequest;
 import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
+import africa.semicolon.cheetah.utils.ModelMapper;
 
 public class PackageServiceImpl implements PackageService {
     private final PackageRepository packageRepository = new PackageRepositoryImpl();
@@ -12,27 +13,15 @@ public class PackageServiceImpl implements PackageService {
     @Override
     public AddPackageResponse addPackage(AddPackageRequest addPackageRequest) {
 //        convert addPackage request to a package
-        Package aPackage = new Package();
-        aPackage.setName(addPackageRequest.getPackageDescription());
-        aPackage.setSenderName(addPackageRequest.getSenderName());
-        aPackage.setSenderPhone(addPackageRequest.getSenderPhone());
-        aPackage.setDeliveryAddress(addPackageRequest.getDeliveryAddress());
-        aPackage.setReceiverName(addPackageRequest.getReceiverName());
-        aPackage.setReceiverPhone(addPackageRequest.getReceiverPhone());
-        aPackage.setNetWeight(addPackageRequest.getPackageWeight());
+        Package aPackage = ModelMapper.map(addPackageRequest);
 
 //        save package
         Package savedPackage = packageRepository.save(aPackage);
 
 //        convert saved package to addPackage response
-        AddPackageResponse response = new AddPackageResponse();
-        response.setPackageName(savedPackage.getName());
-        response.setPackageWeight(savedPackage.getNetWeight());
-        response.setReceiverName(savedPackage.getReceiverName());
-        response.setReceiverPhone(savedPackage.getReceiverPhone());
-        response.setTrackingNumber(savedPackage.getTrackingNumber());
+        AddPackageResponse addPackageResponse = ModelMapper.map(savedPackage);
 
 //        return converted response
-        return response;
+        return addPackageResponse;
     }
 }
