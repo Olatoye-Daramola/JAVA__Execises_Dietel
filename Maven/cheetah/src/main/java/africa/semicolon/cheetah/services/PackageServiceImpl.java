f3d309851aba36a8193e695f5b1e161348622776
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-03 17:07:46 +0100

    ...

--- Maven/cheetah/src/main/java/africa/semicolon/cheetah/services/PackageServiceImpl.java
+++ Maven/cheetah/src/main/java/africa/semicolon/cheetah/services/PackageServiceImpl.java
@@ -1,5 +1,6 @@
 package africa.semicolon.cheetah.services;
 
+import africa.semicolon.cheetah.data.models.Package;
 import africa.semicolon.cheetah.data.repositories.PackageRepository;
 import africa.semicolon.cheetah.data.repositories.PackageRepositoryImpl;
 import africa.semicolon.cheetah.dtos.requests.AddPackageRequest;
@@ -10,6 +11,28 @@ public class PackageServiceImpl implements PackageService {
 
     @Override
     public AddPackageResponse addPackage(AddPackageRequest addPackageRequest) {
-        return null;
+//        convert addPackage request to a package
+        Package aPackage = new Package();
+        aPackage.setName(addPackageRequest.getPackageDescription());
+        aPackage.setSenderName(addPackageRequest.getSenderName());
+        aPackage.setSenderPhone(addPackageRequest.getSenderPhone());
+        aPackage.setDeliveryAddress(addPackageRequest.getDeliveryAddress());
+        aPackage.setReceiverName(addPackageRequest.getReceiverName());
+        aPackage.setReceiverPhone(addPackageRequest.getReceiverPhone());
+        aPackage.setNetWeight(addPackageRequest.getPackageWeight());
+
+//        save package
+        Package savedPackage = packageRepository.save(aPackage);
+
+//        convert saved package to addPackage response
+        AddPackageResponse response = new AddPackageResponse();
+        response.setPackageName(savedPackage.getName());
+        response.setPackageWeight(savedPackage.getNetWeight());
+        response.setReceiverName(savedPackage.getReceiverName());
+        response.setReceiverPhone(savedPackage.getReceiverPhone());
+        response.setTrackingNumber(savedPackage.getTrackingNumber());
+
+//        return converted response
+        return response;
     }
 }
