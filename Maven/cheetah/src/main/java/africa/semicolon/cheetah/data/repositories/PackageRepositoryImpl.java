8f97d947ca4294ef723e5f412c3f22d26c4d1ba0
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-03 17:07:16 +0100

    ...

--- Maven/cheetah/src/main/java/africa/semicolon/cheetah/data/repositories/PackageRepositoryImpl.java
+++ Maven/cheetah/src/main/java/africa/semicolon/cheetah/data/repositories/PackageRepositoryImpl.java
@@ -2,39 +2,45 @@ package africa.semicolon.cheetah.data.repositories;
 
 import africa.semicolon.cheetah.data.models.Package;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class PackageRepositoryImpl implements PackageRepository {
     private final Map<Integer, Package> database = new HashMap<>();
 
     @Override
     public Package save(Package aPackage) {
-        Integer trackingNumber = database.size()+1;
-        aPackage.setTrackingNumber(trackingNumber);
+        Integer trackingNumber = null;
+        if (aPackage.getTrackingNumber() == null) {
+            trackingNumber = database.size() + 1;
+            aPackage.setTrackingNumber(trackingNumber);
+        }
+        trackingNumber = aPackage.getTrackingNumber();
         database.put(trackingNumber, aPackage);
         return database.get(trackingNumber);
     }
 
     @Override
     public List<Package> findAll() {
-        return null;
+        List<Package> all = new ArrayList<>();
+        Set<Integer> keys = database.keySet();
+        for(Integer key : keys) {
+            all.add(database.get(key));
+        }
+        return all;
     }
 
     @Override
     public void delete(Package aPackage) {
-
+        database.remove(aPackage.getTrackingNumber());
     }
 
     @Override
-    public void deleteByTrackingNumber(Integer trackingNumber) {
-
+    public void delete(Integer trackingNumber) {
+        database.remove(trackingNumber);
     }
 
     @Override
-    public Optional<Package> findPackageByTrackingNumber(Integer trackingNumber) {
-        return Optional.empty();
+    public Package findPackageByTrackingNumber(Integer trackingNumber) {
+        return database.get(trackingNumber);
     }
 }
