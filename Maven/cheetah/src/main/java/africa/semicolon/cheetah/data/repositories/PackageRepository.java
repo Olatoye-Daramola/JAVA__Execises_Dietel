efb77c8a02d6ade282ea86ed97a2b25b91b0ca5d
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-03 17:06:05 +0100

    ...

---
+++ Maven/cheetah/src/main/java/africa/semicolon/cheetah/data/repositories/PackageRepository.java
@@ -0,0 +1,13 @@
+package africa.semicolon.cheetah.data.repositories;
+
+import africa.semicolon.cheetah.data.models.Package;
+import java.util.List;
+import java.util.Optional;
+
+public interface PackageRepository {
+    Package save(Package aPackage);
+    List<Package> findAll();
+    void delete(Package aPackage);
+    void delete(Integer trackingNumber);
+    Package findPackageByTrackingNumber(Integer trackingNumber);
+}
