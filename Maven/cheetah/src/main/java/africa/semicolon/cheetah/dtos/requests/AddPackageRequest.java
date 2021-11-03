a3c975667ef92058b0179994d7e0ea3d55de8fa5
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-03 17:06:27 +0100

    ...

---
+++ Maven/cheetah/src/main/java/africa/semicolon/cheetah/dtos/requests/AddPackageRequest.java
@@ -0,0 +1,14 @@
+package africa.semicolon.cheetah.dtos.requests;
+
+import lombok.Data;
+
+@Data
+public class AddPackageRequest {
+    private String senderName;
+    private String senderPhone;
+    private String receiverName;
+    private String receiverPhone;
+    private String deliveryAddress;
+    private double packageWeight;
+    private String packageDescription;
+}
