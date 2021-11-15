20b9130ee54ad0f7024c851d2179fb114b7dd96c
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-15 23:29:20 +0100

    ...

---
+++ Maven/BeansApp/src/main/java/africa/semicolon/BeansApp/utils/ModelMapper.java
@@ -0,0 +1,21 @@
+package africa.semicolon.BeansApp.utils;
+
+import africa.semicolon.BeansApp.data.models.Customer;
+import africa.semicolon.BeansApp.dtos.requests.RegisterCustomerRequest;
+import africa.semicolon.BeansApp.dtos.responses.RegisterCustomerResponse;
+
+public class ModelMapper {
+    public static Customer map(RegisterCustomerRequest registerCustomerRequest) {
+        Customer customer = new Customer();
+        customer.setEmail(registerCustomerRequest.getCustomerEmail());
+        customer.setFullName(registerCustomerRequest.getCustomerFullName());
+        customer.setPassword(registerCustomerRequest.getCustomerPassword());
+        return customer;
+    }
+
+    public static RegisterCustomerResponse map(Customer customer) {
+        RegisterCustomerResponse response = new RegisterCustomerResponse();
+        response.setCustomerEmail(customer.getEmail());
+        return response;
+    }
+}
