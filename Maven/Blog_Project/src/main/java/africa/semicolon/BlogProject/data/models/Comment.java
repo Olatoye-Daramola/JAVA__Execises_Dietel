90152245e8a4703023d2601e826206256b262a9a
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-15 09:32:32 +0100

    ...

--- Maven/Blog_Project/src/main/java/africa/semicolon/BlogProject/data/models/Comment.java
+++ Maven/Blog_Project/src/main/java/africa/semicolon/BlogProject/data/models/Comment.java
@@ -1,14 +1,14 @@
 package africa.semicolon.BlogProject.data.models;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import static java.time.format.DateTimeFormatter.ofPattern;
-
+@Data
 public class Comment {
     private Author author;
     private String body;
     private LocalDateTime timeOfCreation = LocalDateTime.now();
-    private List<Comment> comments;
+//    private List<Comment> comments;
 }
