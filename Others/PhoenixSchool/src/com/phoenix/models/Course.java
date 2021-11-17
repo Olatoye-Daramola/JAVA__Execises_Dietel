2d296b1824940ee851a9aeaad2dadb9e47c7e875
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-17 18:00:43 +0100

    ...

---
+++ Others/PhoenixSchool/src/com/phoenix/models/Course.java
@@ -0,0 +1,19 @@
+package com.phoenix.models;
+
+import lombok.Data;
+
+@Data
+public class Course {
+    private int id;
+    private String courseName;
+    private String department;
+
+    public Course(String name) {
+        this.courseName = name;
+    }
+
+    public Course(String name, String department) {
+        this(name);
+        this.department = department;
+    }
+}
