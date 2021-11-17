6b44648b8cb0813a56e6850a67eef11775dc4599
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-17 18:09:01 +0100

    ...

---
+++ Others/PhoenixSchool/src/com/phoenix/exceptions/PhoenixSchoolException.java
@@ -0,0 +1,15 @@
+package com.phoenix.exceptions;
+
+public class PhoenixSchoolException extends Exception {
+    public PhoenixSchoolException(String message) {
+        super(message);
+    }
+
+    public PhoenixSchoolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PhoenixSchoolException(Throwable cause) {
+        super(cause);
+    }
+}
