d4bcbae0c6267158011b5bb2f1e9016d441adce3
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-19 06:01:08 +0100

    ...

--- Maven/Blog_Project/src/test/java/africa/semicolon/BlogProject/data/repositories/PostRepositoryImplTest.java
+++ Maven/Blog_Project/src/test/java/africa/semicolon/BlogProject/data/repositories/PostRepositoryImplTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostRepositoryImplTest {
@@ -20,38 +22,82 @@ class PostRepositoryImplTest {
     void tearDown() {
     }
 
-    @Test
-    void save() {
-        Author author = new Author();
-
+    public Post testHelper() {
+        Author author = new Author("olatoye", "toye@email.com", "Olatoye David");
         Post post = new Post();
         post.setAuthor(author);
         post.setHeadline("Saving effects of clean code");
         post.setBody("Taciti primis ante ac tellus odio aptent vulputate pretium tincidunt hac habitant");
+        return postRepository.savePost(post);
+    }
 
-        Post savedPost = postRepository.save(post);
+    @Test
+    void save() {
+        Post savedPost = testHelper();
         assertEquals(1, savedPost.getPostId());
         assertEquals(1, postRepository.findAllPosts().size());
     }
 
     @Test
     void updatePost() {
+        Post savedPost = testHelper();
+        String headline = savedPost.getHeadline();
+        String body = """
+                A curae ad sodales nulla accumsan tellus odio lobortis conubia.
+                Per dictum rhoncus rutrum magnis pulvinar consectetur laoreet taciti.
+                """;
+        postRepository.updatePost(savedPost, headline, body);
+
+        assertEquals(1, savedPost.getPostId());
+        assertEquals(1, postRepository.findAllPosts().size());
+        assertEquals(body, savedPost.getBody());
     }
 
     @Test
     void findPostByHeadline() {
+        Post savedPost = testHelper();
+        String headline = "Saving effects of clean code";
+        Post foundPost = postRepository.findPostByHeadline(headline);
+
+        assertEquals(foundPost, savedPost);
     }
 
     @Test
     void findPostByAuthor() {
+        Post savedPost = testHelper();
+        Author author = savedPost.getAuthor();
+        List<Post> foundPosts = postRepository.findPostsByAuthor(author);
+
+        assertTrue(foundPosts.contains(savedPost));
     }
 
     @Test
     void findAllPosts() {
+        Post savedPost = testHelper();
+        assertEquals(1, postRepository.findAllPosts().size());
     }
 
     @Test
-    void deletePostByAuthorUserName() {
+    void deletePostsByAuthorUserEmail() {
+        Author firstAuthor = new Author("olatoye", "toye@email.com", "Olatoye David");
+        Post post = new Post();
+        post.setAuthor(firstAuthor);
+        post.setHeadline("Saving effects of clean code");
+        post.setBody("Taciti primis ante ac tellus odio aptent vulputate pretium tincidunt hac habitant");
+        postRepository.savePost(post);
+
+        Author secondAuthor = new Author("david", "toye_david@email.com", "David Olatoye");
+        Post anotherPost = new Post();
+        anotherPost.setAuthor(secondAuthor);
+        anotherPost.setHeadline("Saving effects of clean code");
+        anotherPost.setBody("Taciti primis ante ac tellus odio aptent vulputate pretium tincidunt hac habitant");
+        postRepository.savePost(anotherPost);
+
+        assertEquals(2, postRepository.findAllPosts().size());
+
+        postRepository.deletePostsByAuthorEmail("toye@email.com");
+        assertEquals(1, postRepository.findAllPosts().size());
+        assertEquals(anotherPost, postRepository.findAllPosts().get(0));
     }
 
     @Test
