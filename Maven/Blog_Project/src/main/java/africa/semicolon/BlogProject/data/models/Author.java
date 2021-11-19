3fb97ddd95f9a983b1c918c619bc8fe9dac3c176
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-19 05:58:37 +0100

    ...

--- Maven/Blog_Project/src/main/java/africa/semicolon/BlogProject/data/models/Author.java
+++ Maven/Blog_Project/src/main/java/africa/semicolon/BlogProject/data/models/Author.java
@@ -1,10 +1,12 @@
 package africa.semicolon.BlogProject.data.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class Author {
     private String userName;
     private String userEmail;
