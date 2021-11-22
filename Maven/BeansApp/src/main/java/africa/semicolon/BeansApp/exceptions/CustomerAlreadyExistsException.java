59c26035a69ba6682d879a6379c50a53ef3aced4
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-22 16:34:32 +0100

    ...

--- Maven/BeansApp/src/main/java/africa/semicolon/BeansApp/exceptions/CustomerAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package africa.semicolon.BeansApp.exceptions;
-
-public class CustomerAlreadyExistsException extends BeansAppException{
-    public CustomerAlreadyExistsException(String message) {
-        super(message);
-    }
-}
